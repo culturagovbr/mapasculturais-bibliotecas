8e03e3bdf4ec0bf05dd9c4286b39a81703b24361
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-11-30 20:41:54 -0200

    Adicionando campos de cadastros nas abas

--- layouts/parts/tab-acervo.php
+++ layouts/parts/tab-acervo.php
@@ -1,3 +1,48 @@
 <div id="tab-acervo" class="aba-content">
-    <h3>acervo</h3>
+    <div class="servico">
+        <p>
+            <span class="label">bib_acervo_possuiPolitica:</span>
+            <span class="js-editable" data-edit="bib_acervo_possuiPolitica" data-original-title="bib_acervo_possuiPolitica" data-emptytext="Selecione">
+                <?php echo $entity->bib_acervo_possuiPolitica; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_acervo_numItens:</span>
+            <span class="js-editable" data-edit="bib_acervo_numItens" data-original-title="bib_acervo_numItens" data-emptytext="Selecione">
+                <?php echo $entity->bib_acervo_numItens; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_acervo_tipos:</span>
+            <editable-multiselect entity-property="bib_acervo_tipos" empty-label="Selecione"></editable-multiselect>
+        </p>
+        <p>
+            <span class="label">bib_acervo_sistemaClassificacao:</span>
+            <editable-select entity-property="bib_acervo_sistemaClassificacao" empty-label="Selecione"></editable-select>
+        </p>
+        <p>
+            <span class="label">bib_acervo_cadastro:</span>
+            <span class="js-editable" data-edit="bib_acervo_cadastro" data-original-title="bib_acervo_cadastro" data-emptytext="Selecione">
+                <?php echo $entity->bib_acervo_cadastro; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_acervo_url:</span>
+            <span class="js-editable" data-edit="bib_acervo_url" data-original-title="bib_acervo_url" data-emptytext="Informe">
+                <?php echo $entity->bib_acervo_url; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_acervo_sistemaGerenciamento:</span>
+            <span class="js-editable" data-edit="bib_acervo_sistemaGerenciamento" data-original-title="bib_acervo_sistemaGerenciamento" data-emptytext="Selecione">
+                <?php echo $entity->bib_acervo_sistemaGerenciamento; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_acervo_acessoEstantes:</span>
+            <span class="js-editable" data-edit="bib_acervo_acessoEstantes" data-original-title="bib_acervo_acessoEstantes" data-emptytext="Selecione">
+                <?php echo $entity->bib_acervo_acessoEstantes; ?>
+            </span>
+        </p>
+    </div>
 </div>
