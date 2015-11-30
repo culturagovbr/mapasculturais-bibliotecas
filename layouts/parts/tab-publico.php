1cf3c024fada46c0da4acd67b9d3d412f8040bb2
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-11-30 14:03:28 -0200

    começando interface da aba 'público'

--- layouts/parts/tab-publico.php
+++ layouts/parts/tab-publico.php
@@ -1,3 +1,16 @@
 <div id="tab-publico" class="aba-content">
-    <h3>público</h3>
+    <div class="servico">
+        <p>
+            <span class="label">Faixa etária predominante dos usuários da biblioteca:</span> 
+            <span class="js-editable" data-edit="bib_faixaEtariaPredominante" data-original-title="Faixa etária predominante" data-emptytext="Selecione">
+                <?php echo $entity->genero; ?>
+            </span>
+        </p>
+        
+        <p>
+            <span class="label">A biblioteca trabalha com comunidades ou grupos específicos?:</span> 
+            <editable-multiselect entity-property="bib_comunidades" empty-label="Selecione" allow-other="true"></editable-multiselect>
+        </p>
+        
+    </div>
 </div>
