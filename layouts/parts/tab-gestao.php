ae8bf5f6c951088ccce71d5c3b11fc251c1cdf70
Leandro Monteiro dal Bó <leandro.db@gmail.com>
2015-12-01 15:51:59 -0200

    Tab Gestão campos restantes

--- layouts/parts/tab-gestao.php
+++ layouts/parts/tab-gestao.php
@@ -52,5 +52,57 @@
                 <?php echo $entity->bib_voluntarios_num; ?>
             </span>
         </p>
+        <p>
+            <span class="label">bib_programasGovernamentais:</span>
+            <editable-multiselect entity-property="bib_programasGovernamentais" empty-label="Selecione"></editable-multiselect>
+        </p>
+        <p>
+            <span class="label">bib_comissao_possui:</span>
+            <span class="js-editable" data-edit="bib_comissao_possui" data-original-title="bib_comissao_possui" data-emptytext="Selecione">
+                <?php echo $entity->bib_comissao_possui; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_associacao_possui:</span>
+            <span class="js-editable" data-edit="bib_associacao_possui" data-original-title="bib_associacao_possui" data-emptytext="Selecione">
+                <?php echo $entity->bib_associacao_possui; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_associacao_nome:</span>
+            <span class="js-editable" data-edit="bib_associacao_nome" data-original-title="bib_associacao_nome" data-emptytext="Selecione">
+                <?php echo $entity->bib_associacao_nome; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_associacao_cnpj:</span>
+            <span class="js-editable" data-edit="bib_associacao_cnpj" data-original-title="bib_associacao_cnpj" data-emptytext="Selecione">
+                <?php echo $entity->bib_associacao_cnpj; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_associacao_email:</span>
+            <span class="js-editable" data-edit="bib_associacao_email" data-original-title="bib_associacao_email" data-emptytext="Selecione">
+                <?php echo $entity->bib_associacao_email; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_parcerias_descricao:</span>
+            <span class="js-editable" data-edit="bib_parcerias_descricao" data-original-title="bib_parcerias_descricao" data-emptytext="Selecione">
+                <?php echo $entity->bib_parcerias_descricao; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_estagio_programa:</span>
+            <span class="js-editable" data-edit="bib_estagio_programa" data-original-title="bib_estagio_programa" data-emptytext="Selecione">
+                <?php echo $entity->bib_estagio_programa; ?>
+            </span>
+        </p>
+        <p>
+            <span class="label">bib_participaPMLL:</span>
+            <span class="js-editable" data-edit="bib_participaPMLL" data-original-title="bib_participaPMLL" data-emptytext="Selecione">
+                <?php echo $entity->bib_participaPMLL; ?>
+            </span>
+        </p>
     </div>
 </div>
