9e36ca69c6b09a59e2aa7626b44b0b95485639a1
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2015-12-02 03:29:56 -0200

    refactor da home

---
+++ layouts/parts/home-search.php
@@ -0,0 +1,17 @@
+<?php 
+$class_space = 'MapasCulturais\Entities\Space';
+$num_spaces = $this->getNumEntities($class_space);
+
+$url_search_spaces = $this->searchSpacesUrl;
+?>
+<section id="home-intro" class="js-page-menu-item home-entity clearfix">
+    <div class="box">
+        <h1><?php $this->dict('home: title') ?></h1>
+        <p><?php $this->dict('home: welcome') ?></p>
+        <p>
+        
+        <a href="<?php echo $url_search_spaces ?>">Visite o mapa</a> e navegue pelas <b><?php echo $num_spaces ?> Bibliotecas cadastradas</b>.
+        
+        </p>
+    </div>
+</section>
